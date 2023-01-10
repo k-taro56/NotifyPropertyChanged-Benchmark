dd7ef498a24ad1023be36d74cd52eae4ea241fda
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2023-01-11 00:48:42 +0900

    Add project files.

---
+++ NotifyPropertyChangedBenchmark/Program.cs
@@ -0,0 +1,94 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace NotifyPropertyChangedBenchmark;
+
+internal class Program
+{
+	private static void Main(string[] args)
+	{
+		BenchmarkRunner.Run<Test>();
+	}
+}
+
+[MemoryDiagnoser]
+public class Test
+{
+	private readonly NoCache noCache = new();
+	private readonly Cache cache = new();
+
+	public Test()
+	{
+		// これがないと PropertyChanged イベントが null になるので通知しなくなる。
+		noCache.PropertyChanged += PropertyChanged;
+		cache.PropertyChanged += PropertyChanged;
+	}
+
+	private void PropertyChanged(object? sender, PropertyChangedEventArgs e)
+	{
+		
+	}
+
+	[Benchmark]
+	public void NotUseCach()
+	{
+		noCache.Value = 1;
+	}
+
+	[Benchmark(Baseline = true)]
+	public void UseCach()
+	{
+		cache.Value = 1;
+	}
+}
+
+public class NoCache : INotifyPropertyChanged
+{
+	private int _value;
+
+	public int Value
+	{
+		get => _value;
+		set
+		{
+			_value = value;
+			NotifyPropertyChanged(nameof(Value));
+		}
+	}
+
+	protected void NotifyPropertyChanged([CallerMemberName] string? caller = null)
+	{
+		PropertyChanged?.Invoke(this, new(caller));
+	}
+
+	public event PropertyChangedEventHandler? PropertyChanged;
+}
+
+public class Cache : INotifyPropertyChanged
+{
+	private int _value;
+
+	public int Value
+	{
+		get => _value;
+		set
+		{
+			_value = value;
+			NotifyPropertyChanged(EventArgsCache.ValuePropertyChanged);
+		}
+	}
+
+	protected void NotifyPropertyChanged(PropertyChangedEventArgs e)
+	{
+		PropertyChanged?.Invoke(this, e);
+	}
+
+	public event PropertyChangedEventHandler? PropertyChanged;
+
+	internal static class EventArgsCache
+	{
+		internal static readonly PropertyChangedEventArgs ValuePropertyChanged = new(nameof(Value));
+	}
+}
