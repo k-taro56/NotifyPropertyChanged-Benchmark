3e73722b3d75c4b54fda44638104ebc04a02c341
github-action-benchmark <github@users.noreply.github.com>
2023-10-27 14:06:47 +0000

    add Benchmark (benchmarkdotnet) benchmark result for 4077d92b9416ea25208554fa357c4d65030f71b7

---
+++ data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1698415607146,
+  "repoUrl": "https://github.com/k-taro56/NotifyPropertyChangedBenchmark",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "121674121+k-taro56@users.noreply.github.com",
+            "name": "k-taro56",
+            "username": "k-taro56"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4077d92b9416ea25208554fa357c4d65030f71b7",
+          "message": "Merge pull request #16 from k-taro56/fix/output-file-path\n\nfix output-file-path",
+          "timestamp": "2023-10-27T22:00:57+09:00",
+          "tree_id": "2a5172ab3225d8950546b3a2e5baafd9038abf3d",
+          "url": "https://github.com/k-taro56/NotifyPropertyChangedBenchmark/commit/4077d92b9416ea25208554fa357c4d65030f71b7"
+        },
+        "date": 1698415606477,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "NotifyPropertyChangedBenchmark.Test.NotUseCach",
+            "value": 12.58583444789052,
+            "unit": "ns",
+            "range": "± 0.9631504447362401"
+          },
+          {
+            "name": "NotifyPropertyChangedBenchmark.Test.UseCach",
+            "value": 1.710303474089195,
+            "unit": "ns",
+            "range": "± 0.22606929739260687"
+          }
+        ]
+      }
+    ]
+  }
+}
